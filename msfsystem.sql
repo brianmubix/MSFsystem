599107401c2cc54db49ebf59ec570093bfd72646
BRIAN <BRIAN@BRIAN-MUBIX>
2019-11-13 18:09:06 +0300

    code 8 Nov 2019

--- msfsystem.sql
+++ msfsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2019 at 10:41 AM
+-- Generation Time: Nov 13, 2019 at 04:05 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -21,11 +21,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `msfsystem`
 --
-
 DROP DATABASE IF EXISTS `msfsystem`;
 CREATE DATABASE IF NOT EXISTS `msfsystem` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `msfsystem`;
-
 -- --------------------------------------------------------
 
 --
@@ -46,7 +44,8 @@ CREATE TABLE `offers` (
 INSERT INTO `offers` (`offer_id`, `service_id`, `name`, `price`) VALUES
 (1, 9, 'vehicles', 100000),
 (2, 8, 'puncher', 500),
-(3, 8, 'tire change', 200);
+(3, 8, 'tire change', 200),
+(12, 9, 'Vitz', 200);
 
 -- --------------------------------------------------------
 
@@ -57,16 +56,61 @@ INSERT INTO `offers` (`offer_id`, `service_id`, `name`, `price`) VALUES
 CREATE TABLE `ratings` (
   `rating_id` int(11) NOT NULL,
   `service_id` int(11) NOT NULL,
-  `score` int(11) NOT NULL
+  `requestid` int(11) DEFAULT NULL,
+  `userid` int(11) NOT NULL DEFAULT 1,
+  `score` decimal(10,1) NOT NULL DEFAULT 0.0,
+  `reason` text DEFAULT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ratings`
 --
 
-INSERT INTO `ratings` (`rating_id`, `service_id`, `score`) VALUES
-(1, 8, 3),
-(2, 8, 5);
+INSERT INTO `ratings` (`rating_id`, `service_id`, `requestid`, `userid`, `score`, `reason`, `datetime`) VALUES
+(1, 8, 1, 7, '3.0', 'i was not happy', '2019-11-07 12:02:23'),
+(2, 8, 2, 7, '5.0', 'very happy', '2019-11-07 12:02:23'),
+(3, 3, 6, 7, '3.0', 'Was happy ', '2019-11-07 12:02:23'),
+(4, 8, 5, 7, '5.0', 'Great experience ', '2019-11-07 12:02:23'),
+(5, 3, 6, 7, '4.0', 'Am happy', '2019-11-07 12:02:23'),
+(6, 9, 4, 7, '5.0', 'Will be back again ', '2019-11-07 12:02:23'),
+(7, 13, 1, 7, '4.0', 'Good work', '2019-11-07 12:02:23'),
+(8, 8, 8, 12, '5.0', 'I loved their service.. ', '2019-11-07 12:56:26'),
+(9, 8, 8, 12, '3.5', 'Average Service', '2019-11-07 12:59:04'),
+(10, 8, 9, 13, '5.0', 'Excellent services', '2019-11-12 18:11:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `request_id` int(11) NOT NULL,
+  `serviceid` int(11) NOT NULL,
+  `customerid` int(11) NOT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `status` enum('Inprogress','Complete') NOT NULL DEFAULT 'Inprogress',
+  `rated` enum('Yes','No') NOT NULL DEFAULT 'No',
+  `create_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `request`
+--
+
+INSERT INTO `request` (`request_id`, `serviceid`, `customerid`, `owner_id`, `description`, `price`, `status`, `rated`, `create_at`) VALUES
+(1, 13, 12, NULL, '0', 6788, 'Complete', 'Yes', '2019-11-06 17:43:35'),
+(2, 9, 8, NULL, 'Gfff', 467, 'Inprogress', 'No', '2019-11-06 04:17:21'),
+(3, 3, 12, 6, 'Yytt\nVyuuj', 688, 'Complete', 'No', '2019-11-06 17:37:30'),
+(4, 9, 8, 6, 'Ffff rrr', 123, 'Complete', 'No', '2019-11-07 07:54:41'),
+(5, 8, 8, 6, 'Wheel alignment 200/=\nChange air filter 500/=', 700, 'Inprogress', 'No', '2019-11-07 08:04:42'),
+(6, 3, 8, 6, 'Hdjdjr 200\nFhfrh 500', 700, 'Inprogress', 'No', '2019-11-07 07:54:46'),
+(7, 13, 12, 9, 'Frrr\nRrrttr ttttt trrr rr4', 299, 'Inprogress', 'No', '2019-11-06 19:43:01'),
+(8, 8, 12, 6, 'Diagnosis 300\nPressure 50', 350, 'Complete', 'Yes', '2019-11-07 09:59:03'),
+(9, 8, 13, 6, 'Puncher repiar 200', 200, 'Complete', 'Yes', '2019-11-12 15:11:38');
 
 -- --------------------------------------------------------
 
@@ -99,7 +143,38 @@ INSERT INTO `services` (`service_id`, `ownerid`, `name`, `category`, `descriptio
 (9, 6, 'motorhub Kiambuu', 'Car Dealer', 'desagu will get you a new car', 'kiambu RD', '-0.39769', '36.96087', 'pending', '2019-09-21 13:18:54'),
 (10, 7, 'name', 'Car Park', 'Here is the description', 'location', '-0.39769', '36.96087', 'pending', '2019-09-25 02:18:53'),
 (11, 7, 'Central Garage', 'Garage Station', 'We repair all type of problems that your vehicle may have.', 'Next to nyeri County', '-0.39769', '36.96087', 'pending', '2019-09-25 07:48:44'),
-(12, 7, 'Oilcom', 'Fuel Station', 'Fuel', 'gatitu', '-0.39769', '36.96087', 'pending', '2019-09-25 09:03:53');
+(12, 7, 'Oilcom', 'Fuel Station', 'Fuel', 'gatitu', '-0.39769', '36.96087', 'pending', '2019-09-25 09:03:53'),
+(13, 9, 'new', 'Car Wash', 'All wash', 'near shell', '-0.42160', '36.95407', 'pending', '2019-11-05 18:20:49'),
+(14, 6, 'silver lark', 'Car Park', 'Derived Derived from the research', 'eie', '-0.42030', '36.95234', 'pending', '2019-11-05 18:27:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `updates`
+--
+
+CREATE TABLE `updates` (
+  `update_id` int(11) NOT NULL,
+  `requestid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `updates`
+--
+
+INSERT INTO `updates` (`update_id`, `requestid`, `userid`, `message`, `datetime`) VALUES
+(1, 5, 8, 'Are you done? ', '2019-11-07 07:52:44'),
+(2, 5, 8, 'I need an update', '2019-11-07 07:53:41'),
+(3, 5, 6, 'We are almost come after two hours', '2019-11-07 08:01:36'),
+(4, 8, 6, 'We are woking on you car', '2019-11-07 09:54:01'),
+(5, 8, 12, 'Okay', '2019-11-07 09:54:50'),
+(6, 7, 12, 'hello\n', '2019-11-07 13:23:26'),
+(7, 9, 6, 'We are almost done', '2019-11-12 15:09:31'),
+(8, 9, 6, 'Be ready we will done', '2019-11-12 15:10:03'),
+(9, 9, 13, 'when do I expect delivery', '2019-11-12 15:10:38');
 
 -- --------------------------------------------------------
 
@@ -117,7 +192,7 @@ CREATE TABLE `users` (
   `email` varchar(200) DEFAULT NULL,
   `phone` varchar(50) DEFAULT NULL,
   `profile_image` varchar(200) DEFAULT NULL,
-  `role` enum('owner','admin') DEFAULT 'owner',
+  `role` enum('owner','customer','admin') DEFAULT 'owner',
   `status` enum('pending','approved','rejected') DEFAULT 'pending',
   `lastseen` datetime DEFAULT current_timestamp(),
   `createdate` datetime DEFAULT current_timestamp()
@@ -131,9 +206,11 @@ INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `gender`, `username`, `
 (4, 'admin', 'admin', 'Male', 'admin', 'admin', 'admin@gmail.com', '0700866545', 'ed 20190919112810.jpg', 'admin', 'pending', '2019-09-13 08:51:32', '2019-09-13 08:51:32'),
 (6, 'sally', 'mubix', 'Female', 'u', 'u', 'sally@gmail.com', '0700866545', NULL, 'owner', 'approved', '2019-09-13 08:57:29', '2019-09-13 08:57:29'),
 (7, 'Brian', 'Mubix', 'Male', 'brian', 'm', 'brianmubix@gmail.com1', '0700866545', NULL, 'owner', 'approved', '2019-09-13 12:29:05', '2019-09-13 12:29:05'),
-(8, 'hg', 'hg', 'Male', 'hj', 'h', 'tyg@hjbjh.jk', '67890', NULL, 'owner', 'pending', '2019-09-20 05:05:35', '2019-09-20 05:05:35'),
+(8, 'hg', 'hg', 'Male', 'hj', 'h', 'tyg@hjbjh.jk', '67890', NULL, 'customer', 'approved', '2019-09-20 05:05:35', '2019-09-20 05:05:35'),
 (9, '1', '2', 'Male', '3', '6', 'brianmubix@gmail.com4', '5', NULL, 'owner', 'approved', '2019-09-20 05:06:54', '2019-09-20 05:06:54'),
-(11, 'Paul', 'Kuria', 'Male', 'Gathogo Kuria', 'admin', 'gathogofranc@gmail.com', '0715359119', NULL, 'owner', 'approved', '2019-09-22 12:46:27', '2019-09-22 12:46:27');
+(11, 'Paul', 'Kuria', 'Male', 'Gathogo Kuria', 'admin', 'gathogofranc@gmail.com', '0715359119', NULL, 'owner', 'approved', '2019-09-22 12:46:27', '2019-09-22 12:46:27'),
+(12, 'briia', 'hhu', 'Male', 'c', 'c', 'brianmubix@gmail.com', '0700866545', NULL, 'customer', 'approved', '2019-11-05 12:55:54', '2019-11-05 12:55:54'),
+(13, 'bb', 'bb', 'Male', 'bb', 'bb', 'bb@gmail.com', '0712345678', NULL, 'customer', 'approved', '2019-11-12 18:06:09', '2019-11-12 18:06:09');
 
 --
 -- Indexes for dumped tables
@@ -151,7 +228,17 @@ ALTER TABLE `offers`
 --
 ALTER TABLE `ratings`
   ADD PRIMARY KEY (`rating_id`),
-  ADD KEY `FK_ratings_services` (`service_id`);
+  ADD KEY `FK_ratings_services` (`service_id`),
+  ADD KEY `FK_ratings_request` (`requestid`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `FK_request_services` (`serviceid`),
+  ADD KEY `FK_request_users` (`customerid`),
+  ADD KEY `FK_request_users_2` (`owner_id`);
 
 --
 -- Indexes for table `services`
@@ -159,6 +246,14 @@ ALTER TABLE `ratings`
 ALTER TABLE `services`
   ADD PRIMARY KEY (`service_id`),
   ADD KEY `FK_services_users` (`ownerid`);
+
+--
+-- Indexes for table `updates`
+--
+ALTER TABLE `updates`
+  ADD PRIMARY KEY (`update_id`),
+  ADD KEY `FK_updates_request` (`requestid`),
+  ADD KEY `FK_updates_users` (`userid`);
 
 --
 -- Indexes for table `users`
@@ -176,25 +271,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `offers`
 --
 ALTER TABLE `offers`
-  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `ratings`
 --
 ALTER TABLE `ratings`
-  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `updates`
+--
+ALTER TABLE `updates`
+  MODIFY `update_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
@@ -210,13 +317,29 @@ ALTER TABLE `offers`
 -- Constraints for table `ratings`
 --
 ALTER TABLE `ratings`
+  ADD CONSTRAINT `FK_ratings_request` FOREIGN KEY (`requestid`) REFERENCES `request` (`request_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_ratings_services` FOREIGN KEY (`service_id`) REFERENCES `services` (`service_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `FK_request_services` FOREIGN KEY (`serviceid`) REFERENCES `services` (`service_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_request_users` FOREIGN KEY (`customerid`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_request_users_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `services`
 --
 ALTER TABLE `services`
   ADD CONSTRAINT `FK_services_users` FOREIGN KEY (`ownerid`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `updates`
+--
+ALTER TABLE `updates`
+  ADD CONSTRAINT `FK_updates_request` FOREIGN KEY (`requestid`) REFERENCES `request` (`request_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_updates_users` FOREIGN KEY (`userid`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
